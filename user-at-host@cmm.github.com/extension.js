55034dc563ce11f6d9d78287ab2e69d5ad02abe7
sharpbai <sharpbai@gmail.com>
2024-03-17 00:59:13 +0800

    Update extension.js

--- user-at-host@cmm.github.com/extension.js
+++ user-at-host@cmm.github.com/extension.js
@@ -1,20 +1,27 @@
 /*Derived from Simple-Username.  https://extensions.gnome.org/extension/807/simple-name/*/
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const GLib = imports.gi.GLib;
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import GLib from 'gi://GLib';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-let label;
+export default class UserAtHostExtension extends Extension {
 
-function enable() {
-    label = new St.Button({reactive: false,
-                           can_focus: false,
-                           track_hover: false,
-                           label: GLib.get_user_name() + '@' + GLib.get_host_name()});
-    Main.panel._leftBox.insert_child_at_index(label, 0);
-}
+    constructor(metadata) {
+        super(metadata);
+        this._label = null
+    }
 
-function disable() {
-    Main.panel._leftBox.remove_child(label);
-    label.destroy();
-    label = null;
+    enable() {
+        this._label = new St.Button({reactive: false,
+            can_focus: false,
+            track_hover: false,
+            label: GLib.get_user_name() + '@' + GLib.get_host_name()});
+        Main.panel._rightBox.insert_child_at_index(this._label, 0);    
+    }
+
+    disable() {
+        Main.panel._leftBox.remove_child(this._label);
+        this._label.destroy();
+        this._label = null;
+    }
 }
